7f6af63297c6524bae19eccc7138e0c41eb1b6d6
Kevin <kevin.netherton@gov.bc.ca>
2016-06-02 10:19:29 -0700

    first commit

---
+++ src/LiquorLocator.py
@@ -0,0 +1,15 @@
+'''
+Created on Jun 2, 2016
+
+@author: KJNETHER
+'''
+
+
+class RouteLiquor(object):
+    
+    def __init__(self, location1, location2):
+        self.location1 = location1
+        self.location2 = location2
+        
+    def getRoute(self):
+        
