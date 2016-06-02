64057fa97830274ecb55027658ce73205ef448cc
Kevin <kevin.netherton@gov.bc.ca>
2016-06-02 13:34:05 -0700

    adding to app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
+import LiquorLocator
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
-
-
+#def getLiquorStoreDemo():
+#    demoPoints = [-126.844567, 49.97859, -122.799997, 58.925305]'
+#    LiquorLocator.RouteLiquor()
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
